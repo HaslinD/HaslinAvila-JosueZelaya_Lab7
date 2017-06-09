1e4eca6b0de72d23d2a01495c68ee734841f4021
Josuezguevara <josuezelaya99@hotmail.es>
2017-06-09 14:14:23 -0600

    Cambios 2

--- main.cpp
+++ main.cpp
@@ -24,11 +24,11 @@ int main(){
 	while (!salir){
         switch(menu()){
            	case 1:{
-					           
+
                	break;}
 
 	       	case 2:{
-	                
+
 	           	break;}
 
 	       	case 3:
@@ -47,22 +47,17 @@ int menu(){
         cout << "-----MENU------" << endl
              << "1.- Ejercicio 1" << endl
              << "2.- Ejercicio 2" << endl
-             << "3.- Salir" << endl; 
+             << "3.- Salir" << endl;
 
         cout << " Ingrese una opción: ";
         cin >> opcion;
-
-<<<<<<< HEAD
-  return 0;
-=======
         if (opcion > 0 && opcion < 4)
             valido = true;
         else {
             cout << "La opcion seleccionada es Nula, intente de nuevo ......." << endl;
         }
         cout << "----------------------" << endl;
-                
+
     }while(!valido);
     return opcion;
->>>>>>> c6adeb68060c2070b982269bacfd293c4431402b
 }
